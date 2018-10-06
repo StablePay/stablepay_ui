e3197f7b0e999d9d3b5c6c5275690327dccce4f7
Guillermo Salazar <guillesalazar1983@yahoo.com.ar>
2018-10-06 16:09:25 -0300

    Getting token/eth balance in combobox

--- store/reducers/tokenReducer.js
+++ store/reducers/tokenReducer.js
@@ -2,26 +2,21 @@ export default function () {
     return [
       {
         value: 'Select Token',
-        label: 'Sellect Token',
+        label: 'Select Token',
         src: 'ether2.svg'
       },
       {
         value: 'ETH',
         label: 'ETH',
         src: 'ethereum-brands.svg',
-        token: '1.09'
+        token: ''
       },
       {
         value: 'ZRX',
         label: 'ZRX',
         src: 'zrx.png',
-        token: '1.09'
-      }, 
-    //   {
-    //     value: 'MKR',
-    //     label: 'MKR',
-    //     src: 'maker.png'
-    //   }
+        token: ''
+      }
     ]
   }
 
