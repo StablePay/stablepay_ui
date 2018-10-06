8c54381547b1ed9855e19587470e13112d387e7d
Douglas Molina <doug.molina@coralbytes.com>
2018-10-05 23:26:06 -0700

    add config exports

--- web3/util/configs.js
+++ web3/util/configs.js
@@ -3,26 +3,17 @@
 const { GANACHE_NETWORK_ID, KOVAN_NETWORK_ID, ROPSTEN_NETWORK_ID } = require( './constants');
 
 
-const TX_DEFAULTS = { gas: 400000 };
-const MNEMONIC = 'concert load couple harbor equip island argue ramp clarify fence smart topic';
+export const TX_DEFAULTS = { gas: 400000 };
+export const MNEMONIC = 'concert load couple harbor equip island argue ramp clarify fence smart topic';
 
-const BASE_DERIVATION_PATH = `44'/60'/0'/0`;
-const GANACHE_CONFIGS = {
+export const BASE_DERIVATION_PATH = `44'/60'/0'/0`;
+export const GANACHE_CONFIGS = {
     rpcUrl: 'http://127.0.0.1:8545',
     networkId: GANACHE_NETWORK_ID,
 };
-const KOVAN_CONFIGS = {
+export const KOVAN_CONFIGS = {
     rpcUrl: 'https://kovan.infura.io/',
     networkId: KOVAN_NETWORK_ID,
 };
 
-const NETWORK_CONFIGS = GANACHE_CONFIGS; // or KOVAN_CONFIGS or ROPSTEN_CONFIGS
-
-module.exports = {
-    TX_DEFAULTS,
-    MNEMONIC,
-    BASE_DERIVATION_PATH,
-    GANACHE_CONFIGS,
-    KOVAN_CONFIGS,
-    NETWORK_CONFIGS
-}
+export const NETWORK_CONFIGS = GANACHE_CONFIGS; // or KOVAN_CONFIGS or ROPSTEN_CONFIGS
--- web3/util/constants.js
+++ web3/util/constants.js
@@ -1,28 +1,17 @@
 const { BigNumber } = require( '0x.js');
 
 
-const ONE_SECOND_MS = 1000;
+export const ONE_SECOND_MS = 1000;
 
-const ONE_MINUTE_MS = ONE_SECOND_MS * 60;
+export const ONE_MINUTE_MS = ONE_SECOND_MS * 60;
 
-const TEN_MINUTES_MS = ONE_MINUTE_MS * 10;
+export const TEN_MINUTES_MS = ONE_MINUTE_MS * 10;
 
-const UNLIMITED_ALLOWANCE_IN_BASE_UNITS = new BigNumber(2).pow(256).minus(1);
+export const UNLIMITED_ALLOWANCE_IN_BASE_UNITS = new BigNumber(2).pow(256).minus(1);
 
-const DECIMALS = 18;
-const NULL_ADDRESS = '0x0000000000000000000000000000000000000000';
-const ZERO = new BigNumber(0);
-const GANACHE_NETWORK_ID = 50;
-const KOVAN_NETWORK_ID = 42;
+export const DECIMALS = 18;
+export const NULL_ADDRESS = '0x0000000000000000000000000000000000000000';
+export const ZERO = new BigNumber(0);
+export const GANACHE_NETWORK_ID = 50;
+export const KOVAN_NETWORK_ID = 42;
 
-export {
-    ONE_SECOND_MS,
-    ONE_MINUTE_MS,
-    TEN_MINUTES_MS,
-    UNLIMITED_ALLOWANCE_IN_BASE_UNITS,
-    DECIMALS,
-    NULL_ADDRESS,
-    ZERO,
-    GANACHE_NETWORK_ID,
-    KOVAN_NETWORK_ID
-}
