598d40db88d7929ecfc54519c1455be907ba9f28
Pamela Rivera <pamelarivera@MacBook-Pro-de-Pamela.local>
2018-10-06 02:10:45 -0700

    responsive

--- components/common/TextButton.js
+++ components/common/TextButton.js
@@ -3,50 +3,7 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 
-// const Root = styled.div`
-//     margin-top: 130px;
-//     flex: 1;
-//     text-align: center;
-//     // width: 300;
-//     @media only screen and (max-width: 600px) {
-//      margin-left: 10px;
-//      margin-right: 10px;
-     
-//   }, 
- 
-  
- 
-// `
 
-// const StyledButton = styled(Button)`
-//   && {
-//   background: #2593e8;
-//   border-radius: 3px;
-//   // border: 0;
-//   color: #fff;
-
-//   // padding: 0 50px;
-//   padding-left: 100px;
-//   padding-right: 100px;
-  
-
-//   }, 
- 
-
-// `
-// function TextButtons (props) {
-//   return (
-//     <Root>
-//       <StyledButton 
-//     onClick={props.buttonClick}
-//       variant='contained' 
-//       href={props.href}>
-//         {props.name}
-//       </StyledButton>
-
-//     </Root>
-//   )
-// }
 const styles = theme => ({
   button: {
     margin: theme.spacing.unit,
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import Link from 'next/link'
+
 import { withStyles } from '@material-ui/core/styles';
 import Head from '../components/head'
-import Nav from '../components/nav'
+
 import TextButton from '../components/common/TextButton'
 import LogoImage from '../components/Home/LogoImage'
 import LogoName from '../components/Home/LogoName'
@@ -31,9 +31,9 @@ function Home (props) {
 return(
   <div>
     <Head title="Home" />
-    {/* <Nav /> */}
+ 
 
-    <div className="hero">
+    <div>
     <div className={classes.logoImage}>    <LogoImage /></div>
     <div className={classes.logoName}><LogoName /></div>
 
@@ -45,10 +45,7 @@ return(
     </div>
 
     <style jsx>{`
-      .hero {
-        width: 100%;
-        color: #333;
-      }
+   
       .title {
         margin: 0;
         width: 100%;
@@ -60,35 +57,7 @@ return(
       .description {
         text-align: center;
       }
-      .row {
-        max-width: 880px;
-        margin: 80px auto 40px;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-around;
-      }
-      .card {
-        padding: 18px 18px 24px;
-        width: 220px;
-        text-align: left;
-        text-decoration: none;
-        color: #434343;
-        border: 1px solid #9b9b9b;
-      }
-      .card:hover {
-        border-color: #067df7;
-      }
-      .card h3 {
-        margin: 0;
-        color: #067df7;
-        font-size: 18px;
-      }
-      .card p {
-        margin: 0;
-        padding: 12px 0 0;
-        font-size: 13px;
-        color: #333;
-      }
+     
     `}</style>
   </div>
 )
